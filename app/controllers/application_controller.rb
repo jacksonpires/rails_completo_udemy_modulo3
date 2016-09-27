1f28c1dba511c2588a6eb8f338b2103f251ee9e9
Jackson Pires <jackson.pires@gmail.com>
2016-09-27 23:53:46 +0000

    Aula 020

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "backoffice_devise"
+    else
+      "application"
+    end
+  end
 end
