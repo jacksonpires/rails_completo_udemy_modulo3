255291c0e489f0b84201631a3f06067103090797
Jackson Pires <jackson.pires@gmail.com>
2016-10-04 20:46:52 +0000

    Aula 023

--- Gemfile
+++ Gemfile
@@ -12,8 +12,7 @@ gem 'devise'
 gem 'devise-i18n'
 # Rails gem of the Bootstrap based admin theme SB Admin 2.
 gem 'bootstrap_sb_admin_base_v2'
-# Use sqlite3 as the database for Active Record
-
+# Use sqlite3 as the database for Active Record'
 gem 'rails-assets-bootstrap', '3.3.7', source: 'https://rails-assets.org'
 
 gem 'sqlite3'
--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -4,14 +4,27 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(params_category)
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi cadastrada com sucesso!"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def params_category
+    params.require(:category).permit(:description)
   end
 end
