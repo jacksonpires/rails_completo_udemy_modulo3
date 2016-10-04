7a67c392a5d81ce0e02b5597835a5d7e37a80021
Jackson Pires <jackson.pires@gmail.com>
2016-10-04 21:20:34 +0000

    Aula 024

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -20,11 +22,20 @@ class Backoffice::CategoriesController < BackofficeController
   end
 
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi atualizada com sucesso!"
+    else
+      render :edit
+    end
   end
 
   private
 
-  def params_category
-    params.require(:category).permit(:description)
-  end
+    def set_category
+      @category = Category.find(params[:id])
+    end
+
+    def params_category
+      params.require(:category).permit(:description)
+    end
 end
