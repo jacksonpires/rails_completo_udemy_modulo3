08948ed1079f328d8c70ab910d80d59bfba3f693
Jackson Pires <jackson.pires@gmail.com>
2016-11-02 01:22:32 +0000

    Aula 042

--- app/helpers/backoffice/admins_helper.rb
+++ app/helpers/backoffice/admins_helper.rb
@@ -1,2 +1,13 @@
 module Backoffice::AdminsHelper
+  OptionsForRoles = Struct.new(:id, :description)
+
+  def options_for_roles
+    options = []
+
+    Admin.roles_i18n.each do |key, value|
+      options.push(OptionsForRoles.new(key, value))
+    end
+
+    options
+  end
 end
