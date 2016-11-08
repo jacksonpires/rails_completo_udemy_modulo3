8a234ddd321b9f549c44a491fcf06ed27a0772b8
Jackson Pires <jackson.pires@gmail.com>
2016-11-08 23:34:16 +0000

    ÃAula 047

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,3 +1,15 @@
 class Backoffice::SendMailController < ApplicationController
 
+  def edit
+    @admin = Admin.find(params[:id])
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    #
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'backoffice', to: 'backoffice/dashboard#index'
 
   namespace :backoffice do
+    resources :send_mail, only: [:edit, :create]
     resources :categories, except: [:show, :destroy]
     resources :admins, except: [:show]
     get 'dashboard', to: 'dashboard#index'
