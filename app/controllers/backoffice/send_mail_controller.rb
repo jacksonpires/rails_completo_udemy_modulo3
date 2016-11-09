ec3e33d80da0f0081a4d1d746b80db999631ea54
Jackson Pires <jackson.pires@gmail.com>
2016-11-09 00:45:51 +0000

    Aula 048

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -9,7 +9,11 @@ class Backoffice::SendMailController < ApplicationController
   end
 
   def create
-    #
+    AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-text'], params['message-text']).deliver_now
+
+    respond_to do |format|
+      format.js
+    end
   end
 
 end
--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,4 +5,12 @@ class AdminMailer < ApplicationMailer
     @admin = admin
     mail(to: @admin.email, subject: "Seus dados foram alterados!")
   end
+
+  def send_message(current_admin, recipient, subject, message)
+    @current_admin = current_admin
+    @recipient = recipient
+    @subject = subject
+    @message = message
+    mail(to: @recipient, subject: @subject)
+  end
 end
