3021a2e85efca997e457a6b81518c2513f82f9c6
Jackson Pires <jackson.pires@gmail.com>
2016-11-21 22:18:33 +0000

    Aula 051

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -58,7 +58,11 @@ class Backoffice::AdminsController < BackofficeController
         params[:admin].except!(:password, :password_confirmation)
       end
 
-      params.require(:admin).permit(policy(@admin).permitted_attributes)
+      if @admin.blank?
+        params.require(:admin).permit(:name, :email, :role, :password, :password_confirmation)
+      else
+        params.require(:admin).permit(policy(@admin).permitted_attributes)
+      end
     end
 
     def password_blank?
