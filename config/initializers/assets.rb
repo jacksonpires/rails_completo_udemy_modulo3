6056b4a2983df6cb36773945a8b9fd97c5590623
Jackson Pires <jackson.pires@gmail.com>
2016-11-29 22:07:19 +0000

    Aula 062

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( site.js backoffice.js )
-Rails.application.config.assets.precompile += %w( site.css backoffice.css )
+Rails.application.config.assets.precompile += %w( site/profile.js site.js backoffice.js )
+Rails.application.config.assets.precompile += %w( site/profile.css site.css backoffice.css )
--- lib/tasks/utils.rake
+++ lib/tasks/utils.rake
@@ -2,9 +2,12 @@ namespace :utils do
 
   desc "Setup Development"
   task setup_dev: :environment do
+    images_path = Rails.root.join('public','system')
+
     puts "Executando o setup para desenvolvimento..."
 
     puts "APAGANDO BD... #{%x(rake db:drop)}"
+    puts "Apagando imagens de public/system #{%x(rm -rf #{images_path})}"
     puts "CRIANDO BD... #{%x(rake db:create)}"
     puts %x(rake db:migrate)
     puts %x(rake db:seed)
