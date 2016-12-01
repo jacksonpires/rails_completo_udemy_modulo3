a4e622a763b2381a21dedc4f7665fcea71a0236b
Jackson Pires <jackson.pires@gmail.com>
2016-12-01 01:40:58 +0000

    Aula 065

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,17 @@
 class Site::Profile::AdsController < Site::ProfileController
+  before_action :set_ad, only: [:edit]
+
   def index
     @ads = Ad.to_the(current_member)
   end
+
+  def edit
+    # Se Ad via before_action
+  end
+
+  private
+
+    def set_ad
+      @ad = Ad.find(params[:id])
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,7 @@ Rails.application.routes.draw do
 
     namespace :profile do
       resources :dashboard, only: [:index]
-    end
-
-    namespace :profile do
-      resources :ads, only: [:index]
+      resources :ads, only: [:index, :edit, :update]
     end
   end
 
