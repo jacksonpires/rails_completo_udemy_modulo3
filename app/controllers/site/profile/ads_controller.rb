5339f7d84aac7f75a8ccec444fbe1e4fcb1936f0
Jackson Pires <jackson.pires@gmail.com>
2016-12-06 00:39:22 +0000

    Aula 067

--- app/controllers/site/profile/ads_controller.rb
+++ app/controllers/site/profile/ads_controller.rb
@@ -1,5 +1,5 @@
 class Site::Profile::AdsController < Site::ProfileController
-  before_action :set_ad, only: [:edit]
+  before_action :set_ad, only: [:edit, :update]
 
   def index
     @ads = Ad.to_the(current_member)
@@ -9,9 +9,21 @@ class Site::Profile::AdsController < Site::ProfileController
     # Se Ad via before_action
   end
 
+  def update
+    if @ad.update(params_ad)
+      redirect_to site_profile_ads_path, notice: "Anúncio atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
   private
 
     def set_ad
       @ad = Ad.find(params[:id])
+    end
+
+    def params_ad
+      params.require(:ad).permit(:id, :title, :category_id, :price, :description, :picture)
     end
 end
