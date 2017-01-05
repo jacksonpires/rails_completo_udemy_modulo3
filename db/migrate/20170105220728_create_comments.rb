d2609b7ab19759e51952731e7e6636f2608d8ab6
Jackson Pires <jackson.pires@gmail.com>
2017-01-05 22:22:34 +0000

    Aula 089

---
+++ db/migrate/20170105220728_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+      t.text :body
+      t.references :member, index: true, foreign_key: true
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227233209) do
+ActiveRecord::Schema.define(version: 20170105220728) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 20161227233209) do
   end
 
   add_index "categories", ["slug"], name: "index_categories_on_slug", unique: true
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["member_id"], name: "index_comments_on_member_id"
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
