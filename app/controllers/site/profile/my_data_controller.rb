15ea832485194d969d4a7af6bdc395f4396b7653
Jackson Pires <jackson.pires@gmail.com>
2017-01-20 01:38:30 +0000

    Aula 100

--- app/controllers/site/profile/my_data_controller.rb
+++ app/controllers/site/profile/my_data_controller.rb
@@ -1,6 +1,24 @@
 class Site::Profile::MyDataController < Site::ProfileController
+  before_action :set_profile_member, only: [:edit, :update]
 
   def edit
-    @profile_member = ProfileMember.find_or_create_by(member_id: current_member.id)
   end
+
+  def update
+    if @profile_member.update(params_profile_member)
+      redirect_to edit_site_profile_my_datum_path(current_member.id), notice: "Perfil atualizado com sucesso!"
+    else
+      render :edit
+    end
+  end
+
+  private
+
+    def set_profile_member
+      @profile_member = ProfileMember.find_or_create_by(member_id: current_member.id)
+    end
+
+    def params_profile_member
+      params.require(:profile_member).permit(:id, :first_name, :second_name, :birthdate)
+    end
 end
