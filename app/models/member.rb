a0217c8ddfb6c089ae013d3e08f1c99d8f47aa9d
Jackson Pires <jackson.pires@gmail.com>
2017-01-28 00:25:14 +0000

    Aula 105

--- app/models/member.rb
+++ app/models/member.rb
@@ -8,8 +8,21 @@ class Member < ActiveRecord::Base
   has_one :profile_member
   accepts_nested_attributes_for :profile_member
 
+  # Validations
+  validate :nested_attributes
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def nested_attributes
+    if nested_attributes_blank?
+      errors.add(:base, "É necessário preencher os campos Primeiro e Segundo nome.")
+    end
+  end
+
+  def nested_attributes_blank?
+    profile_member.first_name.blank? || profile_member.second_name.blank?
+  end
 end
