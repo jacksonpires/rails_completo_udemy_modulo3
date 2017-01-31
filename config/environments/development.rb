8e8bfc506b872f86faea088400a36f8c6b149e69
Jackson Pires <jackson.pires@gmail.com>
2017-01-31 21:05:12 +0000

    Aula 107

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.web_console.whitelisted_ips = ENV['TRUSTED_IP']
 
   # Devise Config
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # MailCatcher
   config.action_mailer.delivery_method = :smtp
