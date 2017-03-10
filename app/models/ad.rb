5e1cc99e9dbccffa6ca26c0feb7303492e1c4152
Jackson Pires <jackson.pires@gmail.com>
2017-03-10 00:31:45 +0000

    Ajuste RAND() MySQL.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -31,7 +31,13 @@ class Ad < ActiveRecord::Base
   scope :to_the, ->(member) { where(member: member) }
   scope :by_category, ->(id, page) { where(category: id).page(page).per(QTT_PER_PAGE) }
 
-  scope :random, ->(quantity) { limit(quantity).order("RANDOM()") }
+  scope :random, ->(quantity) {
+    Rails.env.production?
+      limit(quantity).order("RAND()") # MySQL
+    else
+      limit(quantity).order("RANDOM()") # SQLite
+    end
+  }
 
   # Paperclip
   has_attached_file :picture, styles: { large: "800x300#", medium: "320x150#", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
