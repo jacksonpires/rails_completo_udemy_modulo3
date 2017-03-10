3b7b9aad207dc1814548337a514593fa1f1535c6
Jackson Pires <jackson.pires@gmail.com>
2017-03-10 00:43:03 +0000

    Ajuste RAND() MySQL.

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -32,7 +32,7 @@ class Ad < ActiveRecord::Base
   scope :by_category, ->(id, page) { where(category: id).page(page).per(QTT_PER_PAGE) }
 
   scope :random, ->(quantity) {
-    Rails.env.production?
+    if Rails.env.production?
       limit(quantity).order("RAND()") # MySQL
     else
       limit(quantity).order("RANDOM()") # SQLite
