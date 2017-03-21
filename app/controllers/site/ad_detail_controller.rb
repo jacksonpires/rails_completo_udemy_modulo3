7cb3e41b593516413a7519d555c2de50ecfe5119
Jackson Pires <jackson.pires@gmail.com>
2017-03-21 00:41:42 +0000

    Respondendo aos formatos (JSON / XML)

--- app/controllers/site/ad_detail_controller.rb
+++ app/controllers/site/ad_detail_controller.rb
@@ -3,5 +3,11 @@ class Site::AdDetailController < SiteController
   def show
     @categories = Category.order_by_description
     @ad = Ad.find(params[:id])
+
+    respond_to do |format|
+      format.html # views html.erb
+      format.json { render json: @ad, except: [:description, :description_md] }
+      format.xml { render xml: @ad }
+    end
   end
 end
