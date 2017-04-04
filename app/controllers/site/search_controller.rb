c8bbd141c8b5a37ba2b4c09d0e6272d0cec1859d
Jackson Pires <jackson.pires@gmail.com>
2017-04-04 01:29:05 +0000

    Conhecendo os Cookies

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,7 +1,12 @@
 class Site::SearchController < SiteController
 
   def ads
+    cookies[:search_term] = params[:q]
+
     @ads = Ad.search(params[:q], params[:page])
     @categories = Category.all
+
+    cookies[:categories] = @categories.to_json
+    puts ">>>>>>>>>>>>>>>> #{cookies[:categories]}"
   end
 end
--- app/models/ad.rb
+++ app/models/ad.rb
@@ -24,11 +24,11 @@ class Ad < ActiveRecord::Base
     order(created_at: :desc).page(page).per(QTT_PER_PAGE)
   }
 
-  scope :search, ->(term) {
+  scope :search, ->(term, page) {
     where("lower(title) LIKE ?", "%#{term.downcase}%").page(page).per(QTT_PER_PAGE)
   }
 
-  scope :to_the, ->(member) { where(member: member) }
+  scope :to_the, ->(member, page) { where(member: member) }
   scope :by_category, ->(id, page) { where(category: id).page(page).per(QTT_PER_PAGE) }
 
   scope :random, ->(quantity) {
