fd7707028337f865642d12f97ce0566770392f8a
Jackson Pires <jackson.pires@gmail.com>
2017-04-06 22:36:32 +0000

    Sessions

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -1,5 +1,8 @@
 class Site::HomeController < SiteController
   def index
+    session[:name] = "Jackson"
+    puts ">>>>>>>>>>>> #{session[:name]}"
+
     @categories = Category.order_by_description
     @ads = Ad.descending_order(params[:page])
     @carousel = Ad.random(3)
